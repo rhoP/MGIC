e2da7d2dd6f8b6b5a90e9b96cc02788e2585c32c
rhoP <poch.rohit@gmail.com>
2022-03-04 08:36:29 +0100

    added scripts fro cifar10

---
+++ CIFAR10/MGICNet56.py
@@ -0,0 +1,108 @@
+from __future__ import division
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch import optim
+import numpy as np
+from mgic.mgic import Net56
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+loss_fun = nn.CrossEntropyLoss()
+
+
+def get_data(batch_size):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.49140054, 0.48215815, 0.44652817],
+                             std=[0.24703224, 0.24348514, 0.26158786]),
+    ])
+
+    trainset = datasets.CIFAR10(root='../../data', train=True,
+                                download=True, transform=transform)
+    testset = datasets.CIFAR10(root='../../data', train=False,
+                               download=True, transform=transform)
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    test_loader = DataLoader(testset, batch_size=batch_size, num_workers=8, pin_memory=True)
+    return train_loader, test_loader
+
+
+def train(epochs, train_loader, test_loader):
+    loss_val = []
+    loss_train = []
+    avg_loss_train = []
+    accur = []
+    accur_train = []
+    model = Net56(10, 3)
+    # model = ResNet18()
+    # model = models.vgg16()
+    opt = optim.Adam(model.parameters(), lr=0.001,
+                     betas=(0.9, 0.999), eps=1e-08,
+                     weight_decay=0, amsgrad=True
+                     )
+    sch = torch.optim.lr_scheduler.StepLR(opt, step_size=3, gamma=0.1)
+    model.to(device)
+
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_train_loss = 0.
+
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.float(), target.to(dtype=torch.long)
+            data, target = data.to(device), target.to(device)
+            opt.zero_grad()
+            output = model(data)
+            loss = loss_fun(data, target)
+            total_train_loss += loss * len(data)
+            loss_train.append(loss)
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct = pred.eq(target.view_as(pred)).sum().item()
+            accur_train.append(correct / len(data))
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss))
+
+        model.eval()
+        test_loss = 0.
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.float(), target.to(dtype=torch.long)
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                test_loss += loss_fun(output, target).item() * len(data)  # sum up batch loss
+                pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+                correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+        loss_val.append(test_loss)
+        avg_loss_train.append(total_train_loss / len(train_loader.dataset))
+        test_acc = correct / len(test_loader.dataset)
+        accur.append(test_acc)
+        sch.step(test_loss)
+        print('\nTrain set: Average loss: {:.8f}\n'.format(total_train_loss / len(train_loader.dataset)))
+
+        print('\nTest set: Average loss: {:.8f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+    return loss_val, loss_train, avg_loss_train, accur, accur_train
+
+
+def main():
+    bs = 64
+    t_l, tst_l = get_data(bs)
+    eps = 100
+
+    l_v, l_t, al_t, acc, acct = train(eps, t_l, tst_l)
+
+    np.save("./validation_losses.txt", np.asarray(l_v))
+    np.save("./training_losses.txt", np.asarray(l_t))
+    np.save("./average_training_losses.txt", np.asarray(al_t))
+    np.save("./accuracy.txt", np.asarray(acc))
+    np.save("./accuracy_training.txt", np.asarray(acct))
+
+
+if __name__ == '__main__':
+    main()
--- mgic/mgic.py
+++ mgic/mgic.py
@@ -41,7 +41,7 @@ class Prol2d(nn.Module):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_channels: int, seq: tuple, kernel=(3, 3)):
+    def __init__(self, in_channels: int, planes: int, seq: tuple, kernel=(3, 3), stride=1):
         super(BasicBlock, self).__init__()
         self.cin = in_channels
         self.sg = seq[0]
@@ -56,16 +56,21 @@ class BasicBlock(nn.Module):
         level_channels = [int(self.cin // math.prod(seq[:level]))
                           for level in range(self.n_levels + 1)]
 
+        if stride == 1:
+            self.padding = 1
+        else:
+            self.padding = ((stride - 1) * self.cin + self.kernel[0] - stride) // 2
+
         for level in range(self.n_levels):
             self.res_seq.append(Restr2d(level_channels[level], seq[level]))
             self.prol_seq.append(Prol2d(level_channels[level + 1], seq[level]))
 
         for level in range(self.n_levels - 1, -1, -1):
             self.conv_seq.append(nn.Conv2d(level_channels[level], level_channels[level],
-                                           self.kernel, padding=1, groups=seq[level]))
+                                           self.kernel, stride=stride, padding=self.padding, groups=seq[level]))
             self.bn_seq.append(nn.BatchNorm2d(level_channels[level]))
 
-        self.convc = nn.Conv2d(level_channels[-1], level_channels[-1], self.kernel, padding=1)
+        self.convc = nn.Conv2d(level_channels[-1], level_channels[-1], self.kernel, stride=stride, padding=self.padding)
 
         self.res_seq = nn.Sequential(*self.res_seq)
         self.conv_seq = nn.Sequential(*self.conv_seq)
@@ -74,33 +79,38 @@ class BasicBlock(nn.Module):
 
     def forward(self, x: torch.Tensor):
         x_seq = [x]
-        temp = x.clone()
+        out = x.clone()
         for layer in self.res_seq:
-            temp = layer(temp)
-            x_seq.append(temp)
-        temp = self.convc(temp)
+            out = layer(out)
+            x_seq.append(out)
+        out = self.convc(out)
 
         for level in range(self.n_levels):
-            temp = x_seq[::-1][level + 1] + self.bn_seq[level](self.prol_seq[::-1][level](temp - x_seq[::-1][level]))
-            temp = self.conv_seq[level](temp)
+            out = x_seq[::-1][level + 1] + self.bn_seq[level](self.prol_seq[::-1][level](out - x_seq[::-1][level]))
+            out = self.conv_seq[level](out)
 
-        return temp
+        return out
 
 
 class Net(nn.Module):
-    def __init__(self, Block, layer_list, num_classes, num_channels=3):
+    def __init__(self, Block, layer_list, level_list, num_classes, num_channels=3):
         super(Net, self).__init__()
         self.in_channels = 64
+        self.level_list = level_list
 
         self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False)
+
         self.batch_norm1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
         self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.layer1 = self._make_layer(Block, layer_list[0], planes=64)
-        self.layer2 = self._make_layer(Block, layer_list[1], planes=128, stride=2)
-        self.layer3 = self._make_layer(Block, layer_list[2], planes=256, stride=2)
-        self.layer4 = self._make_layer(Block, layer_list[3], planes=512, stride=2)
+        self.layer1 = self._make_layer(Block, layer_list[0], in_planes=64, out_planes=64)
+        self.layer2 = self._make_layer(Block, layer_list[1], in_planes=128, out_planes=128, stride=1)
+        self.layer3 = self._make_layer(Block, layer_list[2], in_planes=256, out_planes=256, stride=1)
+        self.layer4 = self._make_layer(Block, layer_list[3], in_planes=512, out_planes=512, stride=1)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * Block.expansion, num_classes)
@@ -108,36 +118,26 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.relu(self.batch_norm1(self.conv1(x)))
         x = self.max_pool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
+        x = self.conv2(self.layer1(x))
+        x = self.conv3(self.layer2(x))
+        x = self.conv4(self.layer3(x))
         x = self.layer4(x)
-
         x = self.avgpool(x)
         x = x.reshape(x.shape[0], -1)
         x = self.fc(x)
 
         return x
 
-    def _make_layer(self, Block, blocks, planes, stride=1):
-        ii_downsample = None
+    def _make_layer(self, Block, blocks, in_planes, out_planes, stride=1):
         layers = []
 
-        if stride != 1 or self.in_channels != planes * Block.expansion:
-            ii_downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, planes * Block.expansion, kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes * Block.expansion)
-            )
+        self.in_channels = out_planes * Block.expansion
 
-        layers.append(Block(self.in_channels, planes, i_downsample=ii_downsample, stride=stride))
-        self.in_channels = planes * Block.expansion
-
-        for i in range(blocks - 1):
-            layers.append(Block(self.in_channels, planes))
+        for i in range(blocks):
+            layers.append(Block(in_planes, out_planes, self.level_list, stride=stride))
 
         return nn.Sequential(*layers)
 
 
-def Net34(num_classes, channels=3):
-    return Net(BasicBlock, [3, 4, 6, 3], num_classes, channels)
+def Net56(num_classes, channels=3):
+    return Net(BasicBlock, [3, 4, 6, 3], [2, 2, 2], num_classes, channels)
